92d29a3759a11eac19ff35ca4adb599ba2222e29
artur9487 <artur.www9487@gmail.com>
2022-10-20 17:31:29 +0200

    Added server changes

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ connection.once('open', () => {
 
 app.use('/', require(path.join(__dirname, 'api', 'endpoinst.js')));
 
-//if (process.env.NODE_ENV === 'production') {
+///if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
